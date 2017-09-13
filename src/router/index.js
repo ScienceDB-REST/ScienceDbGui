935392e5e6efade49b4b94a599993786dd9d73f4
edwards33 <err@inbox.lv>
2017-09-13 17:49:56 +0200

    Bugfix: show parent name in EditView

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import Hello from '@/components/Hello'
 import Users from '@/components/users'
 import UserCreate from '@/components/UserCreateForm'
 import UserEdit from '@/components/UserEditForm'
+import taxons from '@/components/taxons'
+import taxonCreate from '@/components/taxonCreateForm'
+import taxonEdit from '@/components/taxonEditForm'
 
 Vue.use(Router)
 
@@ -17,17 +20,32 @@ export default new Router({
     {
       path: '/users',
       name: 'Users',
-      component: Users,
+      component: Users
     },
     {
       path: '/user/:id',
       name: 'UserEdit',
-      component: UserEdit,
+      component: UserEdit
     },
     {
       path: '/user',
       name: 'UserCreate',
-      component: UserCreate,
+      component: UserCreate
+    },
+    {
+      path: '/taxons',
+      name: 'taxons',
+      component: taxons,
+    },
+    {
+      path: '/taxon/:id',
+      name: 'taxonEdit',
+      component: taxonEdit,
+    },
+    {
+      path: '/taxon',
+      name: 'taxonCreate',
+      component: taxonCreate,
     }
   ]
 })
