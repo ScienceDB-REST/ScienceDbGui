472d1952aa5338c5cde42a860b0f33e48ded2fa0
edwards33 <err@inbox.lv>
2017-09-14 15:28:28 +0200

    BugFix: delete Taxon

--- src/components/taxonCreateForm.vue
+++ src/components/taxonCreateForm.vue
@@ -30,7 +30,7 @@ export default {
   methods: {
     onSubmit() {
       var t = this;
-      var url = 'https:localhost:3000/taxon'
+      var url = 'http://localhost:3000/taxon'
       axios.post(url, t.taxon).then(function (response) {
         t.$router.push('/taxons')
       }).catch( function (error) {
