48cecd3992009a6f12f416250b184d41ae067075
edwards33 <err@inbox.lv>
2017-10-13 17:06:38 +0200

    9 - Implement an extended search Note: works only for Taxon. It is an example of ext. search

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ScienceDbGlobals from './sciencedb-globals'
+import VueBlu from 'vue-blu'
+import 'vue-blu/dist/css/vue-blu.min.css'
+
+Vue.use(VueBlu)
 
 Vue.use(ScienceDbGlobals);
 
